87e49961b5ccabd2c066ae41cf00ae1c168d2735
zhongjunqi <zhongjunqi@iplas.com.cn>
2019-10-15 14:39:34 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home1</router-link> |
+      <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <img src="./assets/logo2.png" alt="">
     </div>
     <router-view/>
   </div>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <img src="../assets/logo2.png" alt="">
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
