f1986b1a41127796163e835d4c701e0363aadbd4
zhongjunqi <zhongjunqi@iplas.com.cn>
2019-10-15 14:30:27 +0800

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,10 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin"); // 复制文件插件
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
+    devServer: {
+        disableHostCheck: true
+    },
     configureWebpack: config => {
         const plugins = [];
         plugins.push(
