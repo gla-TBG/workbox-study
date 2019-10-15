250da554fbcc0a11d9d760dabde3b33723a08515
zhongjunqi <zhongjunqi@iplas.com.cn>
2019-10-15 15:05:48 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <div>
-        <img src="./assets/logo.png" alt="">
+        <img src="./assets/logo2.png" alt="">
       </div>
     </div>
     <router-view/>
