2d1f8b69defe7f6da7884527e6c1820df90d9d7b
zhongjunqi <zhongjunqi@iplas.com.cn>
2019-10-15 14:24:06 +0800

    upload demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <img src="../assets/logo2.png" alt="">
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
